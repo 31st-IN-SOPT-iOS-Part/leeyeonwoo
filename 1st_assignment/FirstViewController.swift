c309f5413b710dc14b559acfa49ad288fb119fb3
YEONOO <yeonoo@YEONOOui-MacBookAir.local>
2022-10-14 14:11:02 +0900

    더 못생겨짐

--- 1st_assignment/FirstViewController.swift
+++ 1st_assignment/FirstViewController.swift
@@ -9,42 +9,41 @@ import UIKit
 
 class FirstViewController: UIViewController {
     
-    
+
+//메인라벨 선언
     private let mainLabel: UILabel = {
-        let label1 = UILabel(frame:CGRect(x:100,y:100,width:250,height:30))
+        let label1 = UILabel()
+        
         label1.text="카카오톡을 시작합니다"
         label1.font = .boldSystemFont(ofSize: 20)
+        label1.textAlignment = .center
         return label1
         
     }()
-    
+
+//서브라벨 선언
     private let subLabel: UILabel = {
-        let label2 = UILabel(frame:CGRect(x:120,y:130,width:250,height:30))
-        label2.text="사용하던 카카오계정이 있다면"
+        let label2 = UILabel()
+        label2.text="사용하던 카카오계정이 있다면 \n 이메일 또는 전화번호로 로그인해 주세요."
+        label2.numberOfLines = 2
+        label2.textAlignment = .center
         label2.font = .systemFont(ofSize:13,weight: .medium)
         return label2
     }()
 
     
-    private let subLabel2: UILabel = {
-        let label3 = UILabel(frame:CGRect(x:100,y:150,width:250,height:30))
-        label3.text="이메일 또는 전화번호로 로그인해 주세요."
-        label3.font = .systemFont(ofSize:13,weight: .medium)
-        return label3
-    }()
-    
-    // 이메일 또는 전화번호
+    // 이메일 또는 전화번호 선언
     private let idTextField: UITextField = {
-        let textField = UITextField(frame: CGRect(x:80,y:280,width:250, height:40))
+        let textField = UITextField()
         textField.borderStyle = .roundedRect
         textField.font = .systemFont(ofSize:18,weight:.medium)
         textField.placeholder = "이메일 또는 전화번호"
         return textField
     }()
     
-    // 비밀번호
+    // 비밀번호 선언
     private let paTextField: UITextField = {
-        let textField = UITextField(frame: CGRect(x:80,y:350,width:250, height:40))
+        let textField = UITextField()
         textField.borderStyle = .roundedRect
         textField.font = .systemFont(ofSize:18,weight:.medium)
         textField.isSecureTextEntry = true
@@ -52,9 +51,9 @@ class FirstViewController: UIViewController {
         return textField
     }()
     
-    // 카카오계정 로그인
+    // 카카오계정 로그인 선언
     private let loginButton: UIButton = {
-        let button = UIButton(frame:CGRect(x:80,y:400,width:250,height:42))
+        let button = UIButton()
         button.setTitle("카카오계정 로그인",for: .normal)
         button.setTitleColor(.black, for: . normal)
         button.backgroundColor = .systemGray6
@@ -63,6 +62,8 @@ class FirstViewController: UIViewController {
         
         return button
     }()
+    
+    //로그인 버튼 누르면 세번째뷰로 넘어가라 !!
     
     @objc
     func modal() {
@@ -75,7 +76,7 @@ class FirstViewController: UIViewController {
     
     
     
-    // 새로운 카카오계정 만들기
+    // 새로운 카카오계정 만들기 버튼 선언
     private let joinButton: UIButton = {
         let button = UIButton(frame:CGRect(x:80,y:460,width:250,height:42))
         button.setTitle("새로운 카카오계정 만들기",for: .normal)
@@ -88,6 +89,9 @@ class FirstViewController: UIViewController {
         return button
     }()
     
+    
+//누르면 두번째 페이지로 넘어가라 !!
+    
     @objc
     func testfunction() {
         let secView = secondViewController()
@@ -97,7 +101,7 @@ class FirstViewController: UIViewController {
     }
     
 
-    // 비밀번호 찾기
+    // 비밀번호 찾기 버튼 선언(동작안함)
     private let lastButton: UIButton = {
         let button = UIButton(frame:CGRect(x:80,y:500,width:250,height:42))
         button.setTitle("카카오계정 또는 비밀번호 찾기",for: .normal)
@@ -105,20 +109,29 @@ class FirstViewController: UIViewController {
         //button.backgroundColor = .systemGray
         return button
     }()
-
+    
+    
+    
+    
+    
+// 자 이제 모든 선언했던 것들이 페이지에 보여져라
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         
-        let components: [Any] = [mainLabel,subLabel,subLabel2,idTextField,paTextField,loginButton,joinButton,lastButton]
+        let components: [Any] = [mainLabel,subLabel,idTextField,paTextField,loginButton,joinButton,lastButton]
         components.forEach{
             view.addSubview($0 as! UIView)
         }
 
-        // Do any additional setup after loading the view.
     }
     
-    // 네비게이션
+    
+    
+    
+    
+    
+    // 여기부터 이해안됨
     private func presentToSecondVC(){
         let secondVC = secondViewController()
         secondVC.modalPresentationStyle = .formSheet
@@ -135,14 +148,5 @@ class FirstViewController: UIViewController {
         // presentToSecondVC()
         pushToSecondVC()
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
